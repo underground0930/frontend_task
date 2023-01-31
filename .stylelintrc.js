4ca818aa63e676fbff2f5f63a80a29996281d387
underground0930 <go.nishiduka.1985@gmail.com>
2023-01-31 19:42:47 +0900

    stylelintの設定変更途中

---
+++ .stylelintrc.js
@@ -0,0 +1,7 @@
+// 参考: 既存プロジェクトをStylelint v14にアップグレードするときに注意すべき点
+// https://flex-box.net/stylelint-v14/
+
+module.exports = {
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier-scss'],
+  rules: {},
+}
