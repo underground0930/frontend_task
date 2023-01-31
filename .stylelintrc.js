9bc825c556f4fa003f1e04734a19a8c0bd39f4b6
underground0930 <go.nishiduka.1985@gmail.com>
2023-01-31 23:11:37 +0900

    stylelintを全体に適用`

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,5 +3,16 @@
 
 module.exports = {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier-scss'],
-  rules: {},
+  rules: {
+    'custom-property-pattern': null,
+    'value-keyword-case': null,
+    'selector-id-pattern': null,
+    'selector-class-pattern': null,
+    'keyframes-name-pattern': null,
+    'scss/at-mixin-pattern': null,
+    'scss/dollar-variable-pattern': null,
+    'scss/percent-placeholder-pattern': null,
+    'scss/at-extend-no-missing-placeholder': null,
+    'scss/at-function-pattern': null,
+  },
 }
